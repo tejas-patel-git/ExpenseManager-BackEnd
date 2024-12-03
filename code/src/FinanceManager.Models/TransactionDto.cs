8c4374928f173438de73928ad6f02f76c10dd9a4
Tejas Patel <56188687+NuKleus1@users.noreply.github.com>
2024-12-04 00:41:04 +0530

    update dto

---
+++ code/src/FinanceManager.Models/TransactionDto.cs
@@ -0,0 +1,11 @@
+﻿namespace FinanceManager.Models
+{
+    public class TransactionDto
+    {
+        public int TransactionID { get; set; }
+        public bool IsExpense { get; set; }
+        public decimal Amount { get; set; }
+        public DateTime Date { get; set; }
+        public string Description { get; set; }
+    }
+}
