11a8cb3bcb7c292fab326ee20e384dcfb1289071
Tejas Patel <56188687+NuKleus1@users.noreply.github.com>
2024-12-22 13:19:24 +0530

    add jwt token validation

---
+++ code/src/FinanceManager.API/Controllers/ApiController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FinanceManager.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    [Authorize]
+    [ProducesResponseType(401)]
+    public class ApiController : ControllerBase
+    {
+        public ApiController()
+        {
+            
+        }
+    }
+}
