7f477f8e06140cbbc9ae63166300b41e103e0d9e
Tejas Patel <56188687+NuKleus1@users.noreply.github.com>
2024-12-25 20:29:55 +0530

    add domain project

---
+++ code/src/FinanceManager.Domain/Models/TransactionDomain.cs
@@ -0,0 +1,40 @@
+﻿namespace FinanceManager.Domain.Models;
+
+public class TransactionDomain
+{
+    /// <summary>
+    /// Gets or sets the unique identifier for the transaction.
+    /// </summary>
+    public int TransactionID { get; set; }
+
+    /// <summary>
+    /// Gets or sets the unique identifier for the user associated with the transaction.
+    /// </summary>
+    public int UserID { get; set; }
+
+    /// <summary>
+    /// Gets or sets a value indicating whether the transaction is an expense.
+    /// </summary>
+    /// <remarks>
+    /// If <c>true</c>, the transaction is an expense; otherwise, it is income.
+    /// </remarks>
+    public bool IsExpense { get; set; }
+
+    /// <summary>
+    /// Gets or sets the monetary amount of the transaction.
+    /// </summary>
+    /// <remarks>
+    /// Stored as a decimal with a precision of 10 and scale of 2.
+    /// </remarks>
+    public decimal Amount { get; set; }
+
+    /// <summary>
+    /// Gets or sets the date when the transaction occurred.
+    /// </summary>
+    public DateTime Date { get; set; }
+
+    /// <summary>
+    /// Gets or sets a description of the transaction.
+    /// </summary>
+    public string? Description { get; set; }
+}
