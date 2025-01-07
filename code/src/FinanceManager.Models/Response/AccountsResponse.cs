811fff455a3a77ca83abc0c36d79b45a3a20bc68
Tejas Patel <56188687+NuKleus1@users.noreply.github.com>
2025-01-08 01:30:27 +0530

    add response model

---
+++ code/src/FinanceManager.Models/Response/AccountsResponse.cs
@@ -0,0 +1,14 @@
+﻿using FinanceManager.Domain.Enums;
+
+namespace FinanceManager.Models.Response
+{
+    public class AccountsResponse
+    {
+        public Guid AccountId { get; set; }
+        public string AccountName { get; set; }
+        public string? AccountNumber { get; set; }
+        public string BankName { get; set; }
+        public string AccountType { get; set; }
+        public decimal Balance { get; set; }
+    }
+}
