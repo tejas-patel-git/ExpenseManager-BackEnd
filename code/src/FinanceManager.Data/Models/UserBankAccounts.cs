4c1dc499b452b2897cbe0394f953be1eb57e7e33
Tejas Patel <56188687+NuKleus1@users.noreply.github.com>
2025-01-08 00:58:45 +0530

    add user bank account models

---
+++ code/src/FinanceManager.Data/Models/UserBankAccounts.cs
@@ -0,0 +1,30 @@
+﻿using FinanceManager.Domain.Abstraction;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FinanceManager.Data.Models
+{
+    public class UserBankAccounts : IEntityModel<Guid>, IAuditableEntity
+    {
+        [Key]
+        public Guid Id { get; set; }
+        
+        [ForeignKey("User")]
+        public string UserId { get; set; }
+        
+        public string? AccountNumber { get; set; }
+        
+        public string AccountName { get; set; }
+
+        public string BankName { get; set; }
+        
+        public string AccountType { get; set; }
+
+        [Column(TypeName = "decimal(10, 2)")]
+        public decimal Balance { get; set; }
+        
+        public DateTime CreatedAt { get; set; }
+        
+        public DateTime UpdatedAt { get; set; }
+    }
+}
---
+++ code/src/FinanceManager.Domain/Util/EnumUtil.cs
@@ -0,0 +1,10 @@
+﻿namespace FinanceManager.Domain.Util
+{
+    public static class EnumUtil
+    {
+        public static T ToEnum<T>(this string value) where T : struct, Enum
+        {
+            return Enum.TryParse(value, true, out T result) ? result : default;
+        }
+    }
+}
