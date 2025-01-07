9e7d3e003e573e861d2e54396cf86a90b91981a1
Tejas Patel <56188687+NuKleus1@users.noreply.github.com>
2025-01-08 01:02:54 +0530

    add accounts controller

---
+++ code/src/FinanceManager.API/Controllers/AccountsController.cs
@@ -0,0 +1,35 @@
+﻿using FinanceManager.Application.Services;
+using FinanceManager.Domain.Abstraction.Mappers;
+using FinanceManager.Domain.Models;
+using FinanceManager.Models.Request;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FinanceManager.API.Controllers
+{
+    public class AccountsController : ApiController
+    {
+        private readonly IAccountsService _accountsService;
+        private readonly IMapper<AccountsRequest, AccountsDomain> _requestDomainMapper;
+
+        public AccountsController(IAccountsService accountsService,
+                                  IMapper<AccountsRequest, AccountsDomain> requestDomainMapper)
+        {
+            _accountsService = accountsService;
+            _requestDomainMapper = requestDomainMapper;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddAccount([FromBody] AccountsRequest accountsRequest)
+        {
+            var userId = GetUserIdOfRequest();
+            if (string.IsNullOrEmpty(userId)) return BadRequest(FailureResponse("User id is missing."));
+
+            var accountsDomain = _requestDomainMapper.Map(accountsRequest);
+            accountsDomain.UserId = userId;
+
+            if (!await _accountsService.AddAccount(accountsDomain)) return Conflict(FailureResponse("User does not exists"));
+
+            return Ok(SuccessResponse("Account added successfully."));
+        }
+    }
+}
