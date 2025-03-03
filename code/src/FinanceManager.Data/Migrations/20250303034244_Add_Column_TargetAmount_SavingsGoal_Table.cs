5e708656429ca7de018f2aca57b2201a2d684e26
Tejas Patel <56188687+NuKleus1@users.noreply.github.com>
2025-03-03 09:13:28 +0530

    add column target amount in savings goal entity

---
+++ code/src/FinanceManager.Data/Migrations/20250303034244_Add_Column_TargetAmount_SavingsGoal_Table.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FinanceManager.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class Add_Column_TargetAmount_SavingsGoal_Table : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "TargetAmount",
+                table: "SavingsGoals",
+                type: "decimal(10,2)",
+                nullable: false,
+                defaultValue: 0m);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TargetAmount",
+                table: "SavingsGoals");
+        }
+    }
+}
